d0c5cb69dda42a2a051844bcfb603cd55fcfb3b4
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-18 16:15:05 +0530

    footer code is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <h1>Welcome to ProShop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   )
 }
