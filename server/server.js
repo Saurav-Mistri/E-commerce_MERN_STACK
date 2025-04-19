74bf3f48410fffc4de40ecdc1df99914c132a145
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-19 23:10:38 +0530

    cutom error handler used

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { PORT } from '../config/index.js';
 import db_connect from './config/database.js';
 import product_routes from './routes/product.js';
+import { not_found, error_handler } from './middleware/errorHandler.js';
 
 const port = PORT;
 // Connect to Database
@@ -14,5 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', product_routes);
+
+app.use(not_found);
+app.use(error_handler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
