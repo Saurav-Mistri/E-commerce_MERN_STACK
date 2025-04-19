f6fbed9ca384dfbbc711646f6a1156732bf10797
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-19 22:12:16 +0530

    express router is used for the fetching product-data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { PORT } from '../config/index.js';
 import db_connect from './config/database.js';
-import products from './data/products.js';
+import product_routes from './routes/product.js';
 
 const port = PORT;
 // Connect to Database
@@ -13,13 +13,6 @@ app.get('/', (req, res) => {
     res.send('API is running...!');
 });
 
-app.get('/api/products', (req, res) => {
-    res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.send(product);
-});
+app.use('/api/products', product_routes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
