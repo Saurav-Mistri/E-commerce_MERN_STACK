b2a04eefc20c422177b452c41282e96a74a0a5f2
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-20 14:28:20 +0530

    loader implemented

--- client/src/pages/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
 import { useGetProductDetailsQuery } from '../slices/product_api_slice';
 
 const ProductDetailsPage = () => {
@@ -15,55 +16,55 @@ const ProductDetailsPage = () => {
       <Link className='btn btn-light my-3' to="/">Go Back</Link>
 
       {isLoading ? (
-        <h2>Loading...!</h2>
-      ): error?(
-        <div>{error?.data?.message||error.error}</div>
-      ):(
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.image} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Description: {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+        <Loader />
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.image} fluid />
+          </Col>
+          <Col md={4}>
             <ListGroup variant='flush'>
               <ListGroup.Item>
-                <Row>
-                  <Col>Price: </Col>
-                  <Col>${product.price}</Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status: </Col>
-                  <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
-                </Row>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className='btn-block' type='button' disabled={product.countInStock === 0}> Add to Cart</Button>
+                Price: ${product.price}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price: </Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status: </Col>
+                    <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button className='btn-block' type='button' disabled={product.countInStock === 0}> Add to Cart</Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
       )}
     </>
   )
 }
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
