96f2fc23887ba29ae2db4a3963fdb9ab211e5cde
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-20 15:13:46 +0530

    locally stored cart items functionality done

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/api_slice';
+import cartSliceReducre from './slices/cart_slice';
 
 const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]:apiSlice.reducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        cart: cartSliceReducre,
     },
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools:true,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 });
 
 export default store;
