144892bad84f14e6482a3fa58f7816695bfc58b2
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-20 12:49:37 +0530

    api-slice file created

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/api_slice';
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        [apiSlice.reducerPath]:apiSlice.reducer,
+    },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools:true,
 });
 
 export default store;
