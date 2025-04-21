7ea654b7d33e53818256f8db425ed93bb82ea73c
Saurav-Mistri <sauravmistri21@gmail.com>
2025-04-21 09:16:40 +0530

    auth-slice code is done to store user credentials

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/api_slice';
 import cartSliceReducre from './slices/cart_slice';
+import authSliceReducer from './slices/auth_slice';
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         cart: cartSliceReducre,
+        auth: authSliceReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
